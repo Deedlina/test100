a25e47aff9169ad82367092db971061564ac167e
deedlina <208697475@qq.com>
2017-09-07 16:40:39 +0800

    demo39_about PDF

---
+++ src/demo39/MyPDF.java
@@ -0,0 +1,15 @@
+package demo39;
+
+/**
+ * <p>Title: 生成PDF文件</p>
+ * <p>Description: 本实例通过使用iText包生成一个表格的PDF文件</p>
+ * <p>Filename: myPDF.java</p>
+ * @author 杜江
+ * @version 1.0
+ */
+public class MyPDF {
+    public static void main(String[] args) {
+        //@See http://code1.okbase.net/codefile/ActionForm.java_2012113012919_69.htm
+        System.out.print("");
+    }
+}
